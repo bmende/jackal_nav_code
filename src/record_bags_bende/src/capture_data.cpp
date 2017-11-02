79026005561377c9991452926f026f59af1dfbc9
Benjamin Mende <bmende@moosehill.com>
2017-11-02 19:15:57 -0400

    add jackal workspace to git. initial 'navigation' code here

---
+++ src/record_bags_bende/src/capture_data.cpp
@@ -0,0 +1,282 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string>
+#include <sstream>
+#include <dirent.h>
+
+#include <eigen3/Eigen/Dense>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <sensor_msgs/Joy.h>
+
+
+
+
+#define O_RDONLY   00
+#define BILLION 1000000000.0L;
+
+using std::stringstream;
+using std::string;
+
+// Globals
+string kBagFileDirectory = "/media/jackal_user/UBUNTU/bagfiles/";
+string kBagNodeNameDirectory = "/home/jackal_user/Jackal/bag_node_name_bmende.txt";
+const int kPrefixLength = 5;
+
+ros::Publisher velocity_command_publisher;
+// ros::Publisher stamped_velocity_command_publisher;
+
+// Helper function that sends velocity commands to the Jackal to signal the
+// start and end of the recording
+void MoveJakcal (bool direction, double duration) {
+    struct timespec ms1;
+    struct timespec ms2;
+    double s_elapsed1;
+
+    geometry_msgs::Twist vel_command;
+    vel_command.linear.x = 0;
+    vel_command.linear.y = 0;
+    vel_command.linear.z = 0;
+    vel_command.angular.x = 0;
+    vel_command.angular.y = 0;
+    float rot_vel = 0.05;
+
+    // rotate left
+    if (direction) {
+        vel_command.linear.x = -rot_vel;
+        // rotate right
+    } else {
+        vel_command.linear.x = rot_vel;
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &ms1);
+    clock_gettime(CLOCK_MONOTONIC, &ms2);
+    s_elapsed1 = (ms2.tv_sec - ms1.tv_sec) + (ms2.tv_nsec - ms1.tv_nsec) / BILLION;
+
+    while (s_elapsed1 < duration) {
+        velocity_command_publisher.publish(vel_command);
+        clock_gettime(CLOCK_MONOTONIC, &ms2);
+        s_elapsed1 = (ms2.tv_sec - ms1.tv_sec) + (ms2.tv_nsec - ms1.tv_nsec) / BILLION;
+    }
+
+}
+
+// Helper function that reads the names of the files in the bag files directory
+// and figures out the name of the next bag file to be saved
+void FindNextFileName (std::string* file_name) {
+
+    char numbering[5];
+    DIR* dirp = opendir(kBagFileDirectory.c_str());
+    int latest_index = 0;
+    struct dirent * dp;
+    int max_prefix_number = 0;
+
+    while ((dp = readdir(dirp)) != NULL){
+
+        // Ignore the '.' and ".." directories
+        if(!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..")) continue;
+        for(int i = 0; i < kPrefixLength ; i++){
+            numbering[i] = dp->d_name[i];
+        }
+
+        int prefix_number = atoi(numbering);
+        if(prefix_number > max_prefix_number) max_prefix_number = prefix_number;
+    }
+    (void)closedir(dirp);
+
+    char cur_prefix_number [5];
+    sprintf(cur_prefix_number, "%05d", max_prefix_number + 1);
+    *file_name = cur_prefix_number;
+    file_name->append(".bag");
+}
+
+// Helper function that wraps the system command so that we can get the pid
+// of the started process
+pid_t system_wrapper(const char * command, int * infp, int * outfp)
+{
+    int p_stdin[2];
+    int p_stdout[2];
+    pid_t pid;
+
+    if (pipe(p_stdin) == -1)
+        return -1;
+
+    if (pipe(p_stdout) == -1) {
+        close(p_stdin[0]);
+        close(p_stdin[1]);
+        return -1;
+    }
+
+    pid = fork();
+
+    if (pid < 0) {
+        close(p_stdin[0]);
+        close(p_stdin[1]);
+        close(p_stdout[0]);
+        close(p_stdout[1]);
+        return pid;
+    } else if (pid == 0) {
+        close(p_stdin[1]);
+        dup2(p_stdin[0], 0);
+        close(p_stdout[0]);
+        dup2(p_stdout[1], 1);
+        dup2(open("/dev/null", O_RDONLY), 2);
+        /// Close all other descriptors for the safety sake.
+        for (int i = 3; i < 4096; ++i)
+            close(i);
+
+        setsid();
+        execl("/bin/sh", "sh", "-c", command, NULL);
+        _exit(1);
+    }
+
+    close(p_stdin[0]);
+    close(p_stdout[1]);
+
+    if (infp == NULL) {
+        close(p_stdin[1]);
+    } else {
+        *infp = p_stdin[1];
+    }
+
+    if (outfp == NULL) {
+        close(p_stdout[0]);
+    } else {
+        *outfp = p_stdout[0];
+    }
+
+    return pid;
+}
+
+// Helper function which starts and stops recording bag files by reading the
+// corresponding signals from the joystick
+void JoystickCallback(const sensor_msgs::Joy::ConstPtr& joy) {
+    static bool recording = false;
+    static bool one_time_rec = false;
+    static pid_t pid;
+
+    int X = joy->buttons[14];
+    int O = joy->buttons[13];
+
+    // If X pressed, start recording
+    if (X) {
+        if (!recording) {
+            recording = true;
+            one_time_rec = true;
+        }
+    }
+
+    // If O is pressed, stop recording
+    if (O) {
+        if (recording) {
+            recording = false;
+            stringstream command;
+            stringstream command1;
+
+            // Use the process id to kill the recording node: Not working, in order for
+            // it to work you should send the command in sudo mode
+
+            // command << "echo 7DigitPrecision | sudo -S kill -9 " << pid;
+            // command << "kill 9 " << pid;
+            // std::cout << command.str().c_str() << std::endl;
+            // system(command.str().c_str());
+
+            // Find out the name of the node recording the bag file and save it to a file
+            command1 << "rosnode list | grep /record_* > " << kBagNodeNameDirectory.c_str();
+            system(command1.str().c_str());
+            std::ifstream in;
+            string record_node_name;
+
+            // Read the name of the recording node from file and kill it
+            // NOTE: The path depends on the user name on the Jackal (jackal_user)
+            in.open (kBagNodeNameDirectory.c_str(), std::ifstream::in);
+            if(!in.is_open()){
+                ROS_INFO("Error openning bag_node_name.txt");
+            } else {
+                if (std::getline(in, record_node_name)) {
+                    command << "rosnode kill " << record_node_name.c_str();
+                    system(command.str().c_str());
+
+                    // After stopping recording, moves the Jackal to signal the end (rotates
+                    // to the left)
+                    MoveJakcal(true, 1.0);
+                }
+                in.close();
+            }
+        }
+    }
+
+    if (one_time_rec) {
+        one_time_rec = false;
+        // system("rosbag record -O ~/Jackal/bag_files/test1.bag "
+        //                      "--duration=10 "
+        // 	"/imu/data /cmd_vel /odometry/filtered &");
+        int infp;
+        int outfp;
+        string bag_file_name;
+        stringstream recording_command;
+        FindNextFileName(&bag_file_name);
+
+        recording_command << "rosbag record -o " << kBagFileDirectory.c_str() <<
+            bag_file_name.c_str() <<
+            " /webcam/left/camera_info /webcam/right/camera_info "
+            " /webcam/left/image_raw/compressed/parameter_descriptions  /webcam/left/image_raw/compressed/parameter_updates "
+            " /webcam/right/image_raw/compressed/parameter_descriptions  /webcam/right/image_raw/compressed/parameter_updates "
+            " /webcam/left/image_raw/compressed "
+            " /webcam/right/image_raw/compressed ";
+
+        // Before starting recording, moves the Jackal to signal the start (rotates
+        // to the right)
+        MoveJakcal(false, 1.0);
+        pid = system_wrapper(recording_command.str().c_str(), &infp, &outfp);
+    }
+    ROS_INFO("Recording status: %d", recording);
+}
+
+
+// void CmdVelCallback(const geometry_msgs::Twist::ConstPtr& cmd) {
+// ros::Time time_stamp = ros::Time::now();
+// geometry_msgs::TwistStamped cmd_vel_stamped;
+
+// cmd_vel_stamped.twist.linear.x = cmd->linear.x;
+// cmd_vel_stamped.twist.linear.y = cmd->linear.y;
+// cmd_vel_stamped.twist.linear.z = cmd->linear.z;
+
+// cmd_vel_stamped.twist.angular.x = cmd->angular.x;
+// cmd_vel_stamped.twist.angular.y = cmd->angular.y;
+// cmd_vel_stamped.twist.angular.z = cmd->angular.z;
+
+// cmd_vel_stamped.header.stamp = time_stamp;
+
+// stamped_velocity_command_publisher.publish(cmd_vel_stamped);
+// }
+
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "capture_data_bende");
+    ros::NodeHandle nh;
+
+    ros::Subscriber joystick_subscriber =
+        nh.subscribe("/bluetooth_teleop/joy",5, JoystickCallback);
+
+    // ros::Subscriber cmd_vel_subscriber =
+    //   nh.subscribe("/cmd_vel",5, CmdVelCallback);
+
+    // stamped_velocity_command_publisher =  nh.advertise<geometry_msgs::TwistStamped>("/cmd_vel_stamped", 5);
+
+    velocity_command_publisher =  nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+    ROS_INFO("Press X to start recording and press O to stop recording.");
+    ROS_INFO("Jackal rotates to the right to signal the start of recording"
+             " and rotates to the left to signal the end of recording.");
+
+    ros::spin();
+    return 0;
+}
---
+++ src/record_bags_bende/src/navigate.cpp
@@ -0,0 +1,263 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include <ctime>
+#include <fstream>
+#include <deque>
+#include <geometry_msgs/Pose.h>
+#include <tf/transform_datatypes.h>
+//#include "popt_pp.h"
+
+using namespace std;
+using namespace cv;
+
+ros::Publisher vel_pub;
+
+double forward_vel = 0., rot_vel = 0.;
+double trans_accel = 0.025; // forward acceleration
+double trans_decel = 0.1; // forward deceleration
+double rot_accel = 0.05; // rotational acceleration
+float max_forward_vel = 1.0; // maximum forward velocity
+double max_rot_vel = 0.6; // maximum rotational velocity
+
+bool triangle_pressed = false;
+
+// declare clearances in front and side of the robot
+double clear_front = 0.24 + 0.8;
+double clear_side = 0.3;
+
+deque< int > commands;
+int last_dir = 0;
+
+const int INF = 1e9;
+
+struct Pose
+{
+    double x, y, theta;
+    double dist(Pose p) {
+        return sqrt((x - p.x)*(x - p.x) + (y - p.y)*(y - p.y));
+    }
+};
+
+struct LineSegment
+{
+    float x1, y1, x2, y2;
+    float getAngle(LineSegment l) {
+        float m1 = atan2(y2-y1,x2-x1);
+        float m2 = atan2(l.y2-l.y1,l.x2-l.x1);
+        return m1 - m2;
+    }
+    float heading() {
+        return atan2(y2-y1,x2-x1);
+    }
+};
+
+Pose jackal_pos = {0,0,0};
+Pose last_jackal_pos = {0,0,0};
+Pose current_waypoint;
+bool reached_waypoint = false;
+deque< Pose > path;
+int pose_update_counter = 0;
+int rot_frames = 0;
+
+
+pair< double, double > goToWayPoint(Pose wayPoint, double front) {
+    pair< double, double > ret_vel;
+    double dist = wayPoint.dist(jackal_pos);
+    if (dist < 0.5) {
+        reached_waypoint = true;
+        cout << "reached waypoint " << wayPoint.x << ", " << wayPoint.y << " : " << jackal_pos.x << ", "<< jackal_pos.y << endl;
+        ret_vel = make_pair(0.,0.);
+    } else if (rot_frames != 0) {
+        if (rot_frames < 0) {
+            cout << "ROTATING LEFT!!!" << endl;
+            ret_vel.second = max_rot_vel * 0.5;
+            rot_frames++;
+        } else {
+            cout << "ROTATING RIGHT!!!" << endl;
+            ret_vel.second = -max_rot_vel * 0.5;
+            rot_frames--;
+        }
+        ret_vel.first = 0.;//max_forward_vel * max(0.4, front);
+    } else {
+        ret_vel.first = max_forward_vel * max(0.4, front);
+        ret_vel.second = 0.;
+    }
+    cout << "Distance to WP: " << dist << endl;
+    return ret_vel;
+}
+
+pair< double, double > autoNavigateMode(double front) {
+    pair< double, double > ret_vel;
+    ret_vel = make_pair(0.,0.);
+    if (path.size() == 0 && reached_waypoint) {
+        cout << "REACHED ALL WAYPOINTS" << endl;
+        return ret_vel;
+    }
+    if (reached_waypoint) {
+        cout << "REACHED WAYPOINT! *********************************" << endl;
+        current_waypoint = path.front();
+        path.pop_front();
+        reached_waypoint = false;
+    }
+    if (!reached_waypoint) {
+        ret_vel = goToWayPoint(current_waypoint, front);
+    }
+    cout << "Current waypoint: " << current_waypoint.x << ", " << current_waypoint.y << endl;
+    return ret_vel;
+}
+
+void safeNavigate(const sensor_msgs::JoyConstPtr& msg) {
+    // read joystick input
+    int R2 = msg->buttons[9];
+    int R1 = msg->buttons[11];
+    int X = msg->buttons[14];
+    int O = msg->buttons[13];
+    int triangle = msg->buttons[12];
+    double side = msg->axes[0];
+    double front = msg->axes[1];
+    double desired_forward_vel, desired_rot_vel;
+    pair< double, double > desired_vel;
+    // run the different modes
+
+    if (triangle) {
+        desired_vel = autoNavigateMode(front); // navigation doesn't work yet
+        triangle_pressed = true;
+    } else {
+        triangle_pressed = false;
+        return;
+    }
+    // accelerate or decelerate accordingly
+    desired_forward_vel = desired_vel.first;
+    desired_rot_vel = desired_vel.second;
+    if (desired_forward_vel < forward_vel) {
+        forward_vel = max(desired_forward_vel, forward_vel - trans_decel);
+    } else {
+        forward_vel = min(desired_forward_vel, forward_vel + trans_accel);
+    }
+    if (desired_rot_vel < rot_vel) {
+        rot_vel = max(desired_rot_vel, rot_vel - rot_accel);
+    } else {
+        rot_vel = min(desired_rot_vel, rot_vel + rot_accel);
+    }
+
+    cout << "Forward Vel: " << forward_vel << ", Rot Vel: " << rot_vel << endl;
+
+    geometry_msgs::Twist vel_msg;
+    vel_msg.linear.x = forward_vel;
+    vel_msg.angular.z = rot_vel;
+    vel_pub.publish(vel_msg);
+}
+
+void getCurrentPose(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+    jackal_pos.x = msg->pose.position.x;
+    jackal_pos.y = msg->pose.position.y;
+
+    // get yaw! first get quaternion, then turn to yaw
+    tf::Quaternion q(msg->pose.orientation.x,
+                     msg->pose.orientation.y,
+                     msg->pose.orientation.z,
+                     msg->pose.orientation.w);
+    tf::Matrix3x3 m(q);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+
+    jackal_pos.theta = yaw;
+    pose_update_counter++;
+    //cout << "Current: " << jackal_pos.x << ", " << jackal_pos.y << " Prev: " << last_jackal_pos.x << ", " << last_jackal_pos.y << endl;
+
+    LineSegment heading_line = {last_jackal_pos.x,last_jackal_pos.y,jackal_pos.x,jackal_pos.y};
+    LineSegment waypoint_line = {jackal_pos.x,jackal_pos.y,current_waypoint.x,current_waypoint.y};
+    //cout << "Heading: " << (heading_line.heading() * 180. / 3.14) << endl;
+    double ang_diff = heading_line.getAngle(waypoint_line);
+    //cout << "Ang diff: " << (ang_diff * 180. / 3.14)  << endl;
+    //cout << "Rot frames: " << rot_frames << endl;
+
+    if (!triangle_pressed)
+        cout << "Current position: " << jackal_pos.x << ", " << jackal_pos.y << ", " << (jackal_pos.theta*180/3.14) << ". AngleDiff: " << (ang_diff*180/3.14) <<endl;
+
+
+    if (pose_update_counter > 20) {
+        if (last_jackal_pos.dist(jackal_pos) > 0.1) {
+            if (abs(ang_diff * 180 / 3.14) > 10) {
+                double cmd_rate = 8.;
+                rot_frames = ang_diff * cmd_rate / (max_rot_vel * 0.5);
+            } else {
+                rot_frames = 0;
+            }
+            last_jackal_pos = jackal_pos;
+        }
+        pose_update_counter = 0;
+    }
+}
+
+void read_waypoints(char* filename) {
+    ifstream f(filename);
+    if (f.is_open()) {
+        int n;
+        Pose waypoint;
+        float x, y;
+        waypoint.theta = 0.;
+        f >> n;
+        for (int i = 0; i < n; i++) {
+            f >> x;
+            f >> y;
+            waypoint.x = x;
+            waypoint.y = y;
+            cout << "Read (" << waypoint.x << "," << waypoint.y << ")" << endl;
+            path.push_back(waypoint);
+        }
+        current_waypoint = path.front();
+        path.pop_front();
+        cout << "Read waypoints!" << endl;
+    } else {
+        cout << "Cannot open file!" << endl;
+    }
+}
+
+void test_waypoints() {
+
+    Pose w1, w2, w3, w4, w5;
+    w1.x = 0.1; w1.y = 0.1; w1.theta = 0.;
+    w2.x = 6.; w2.y = 0.1; w2.theta = 0.;
+    w3.x = 12.; w3.y = 0.1; w3.theta = 0.;
+    w4.x = 28.5; w4.y = 0.1; w4.theta = 0.;
+    w5.x = 28.5; w5.y = -10.1; w5.theta = 0.;
+    path.push_back(w2);
+    path.push_back(w3);
+    path.push_back(w4);
+    path.push_back(w5);
+    current_waypoint = w1;
+
+    cout << "test waypoints added\n";
+
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "orb_naive_navigation");
+    ros::NodeHandle nh;
+
+    // static struct poptOption options[] = {
+    //     { "max-forward-vel",'f',POPT_ARG_FLOAT,&max_forward_vel,0,"Max forward velocity","NUM" },
+    //     { "laser-thresh",'l',POPT_ARG_INT,&laser_pt_thresh,0,"Threshold for obstacle scan","NUM" },
+    //     { "forward-clearance",'c',POPT_ARG_FLOAT,&clear_front,0,
+    //       "Forward clearance range","NUM" },
+    //     POPT_AUTOHELP
+    //     { NULL, 0, 0, NULL, 0, NULL, NULL }
+    // };
+
+    // POpt popt(NULL, argc, argv, options, 0);
+    // int c;
+    // while((c = popt.getNextOpt()) >= 0) {}
+
+    test_waypoints();
+
+    ros::Subscriber sub_safe_drive = nh.subscribe("/bluetooth_teleop/joy", 1, safeNavigate);
+    ros::Subscriber sub_cur_pose = nh.subscribe("/orb_slam2/pose", 1, getCurrentPose);
+    vel_pub = nh.advertise<geometry_msgs::Twist>("/jackal_velocity_controller/cmd_vel", 1);
+    ros::spin();
+    return 0;
+}
